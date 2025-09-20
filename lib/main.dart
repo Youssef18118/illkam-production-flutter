6c205594a3aae988752f3992f8aea5bca795aba9
Youssef18118 <101882887+Youssef18118@users.noreply.github.com>
2025-09-20 17:17:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 
 import 'pages/chat/ChatScreenPage.dart';
@@ -32,7 +31,6 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterLocalization.instance.ensureInitialized(); 
-  await dotenv.load(fileName: ".env");
   await initializeDateFormatting();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
